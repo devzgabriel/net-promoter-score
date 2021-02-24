188e23502ce84491b7279156b1afc7d0818d3d28
Gabriel Silva <devzgabriel@gmail.com>
2021-02-24 18:37:48 -0300

    surveys routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { UserController } from "./controllers/UserControler";
 import { Router } from "express";
+import { SurveysController } from "./controllers/SurveysController";
 
 const router = Router();
-const userController = new UserController();
-router.post("/users", userController.create);
+const usersController = new UserController();
+const surveysController = new SurveysController();
+
+router.post("/users", usersController.create);
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
