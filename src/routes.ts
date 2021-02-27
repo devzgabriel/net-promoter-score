b7323624a73e2a7481e680d1279937b4893fc58f
Gabriel Silva <devzgabriel@gmail.com>
2021-02-27 11:18:10 -0300

    calculate nps

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import { SurveysController } from "./controllers/SurveysController";
 import { SendMailController } from "./controllers/SendMailController";
 import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 
 const router = Router();
 const usersController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", usersController.create);
 
@@ -18,5 +20,6 @@ router.get("/surveys", surveysController.show);
 router.post("/sendMail", sendMailController.execute);
 
 router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
